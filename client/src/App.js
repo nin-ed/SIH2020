cd4c1fec5c7ebcab5e9e2002bb133d3a93e95485
princebansal42 <princebansal42@gmail.com>
2020-01-10 21:40:14 +0530

    Basic Redux Boilerplate Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import {Route, Switch, Redirect} from "react-router-dom";
-import { Container } from 'reactstrap';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import Home from './components/Home';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Container } from "reactstrap";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <Container>
-    <Switch>
-      <Route path="/home" component={Home}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/Signup" component={Signup}/>
-      <Redirect to="/home"/>
-    </Switch>
-    </Container>
-  );
-}
+// Redux
+import store from "./store";
+import { Provider } from "react-redux";
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Container>
+                <Switch>
+                    <Route path='/home' component={Home} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/Signup' component={Signup} />
+                    <Redirect to='/home' />
+                </Switch>
+            </Container>
+        </Provider>
+    );
+};
 
 export default App;
